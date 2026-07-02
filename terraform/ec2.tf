dbac21c1e73c791df7a446b4aebd2402db662b19
Felixchimaa <129009171+Felixchimaa@users.noreply.github.com>
2026-07-02 10:24:33 +0100

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "drupal_server_1" {
 
 resource "aws_instance" "drupal_server_2" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public_subnet_2.id
   vpc_security_group_ids      = [aws_security_group.drupal_sg.id]
   key_name                    = "Goodness"
